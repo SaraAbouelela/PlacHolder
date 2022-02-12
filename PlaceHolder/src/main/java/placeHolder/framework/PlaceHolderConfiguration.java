4d45852f954cc8eab595ec7704bbaac58599716e
SaraAbouelela <84548573+SaraAbouelela@users.noreply.github.com>
2022-02-13 00:28:49 +0200

    pushing the full implementation with the extent report

--- PlaceHolder/src/main/java/placeHolder/framework/PlaceHolderConfiguration.java
+++ PlaceHolder/src/main/java/placeHolder/framework/PlaceHolderConfiguration.java
@@ -1,16 +1,11 @@
 package placeHolder.framework;
 
-//import io.restassured.RestAssured;
-import org.testng.annotations.*;
 import io.restassured.RestAssured;
+import org.testng.annotations.BeforeMethod;
 
-public class PlaceHolderConfiguration {
-	
-	@BeforeSuite (alwaysRun = true)
+public class PlaceHolderConfiguration{
+	@BeforeMethod(alwaysRun = true)
 	public void configure() {
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-
 	}
-	
-
 }
--- PlaceHolder/src/test/java/PlaceHolder/common/RestAssuredConfiguration.java
+++ PlaceHolder/src/test/java/PlaceHolder/common/RestAssuredConfiguration.java
@@ -3,7 +3,6 @@ package PlaceHolder.common;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 public class RestAssuredConfiguration {
@@ -11,6 +10,7 @@ public class RestAssuredConfiguration {
     @BeforeSuite(alwaysRun = true)
     public void configure()
     {
+        System.out.println("In the Configre method");
         RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
     }
 
